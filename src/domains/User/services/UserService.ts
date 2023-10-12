b42934245940189c13a4b99c879411fc91a10955
Maria Eduarda <dudah.s3@gmail.com>
2023-10-12 19:17:29 -0300

    ajusta create de User e config de eslint

--- src/domains/User/services/UserService.ts
+++ src/domains/User/services/UserService.ts
@@ -1,19 +1,19 @@
 import prisma from '../../../../client/client'
-
+import { User } from '@prisma/client'
 class UserService {
-    async create(body: any) {
-        const user = await prisma.user.create({
-            data: {
-                email: body.email,
-                name: body.name,
-                password: body.password,
-                photo: body.photo,
-                role: body.role,
-            }
-        });
+	async create(body: User) {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				name: body.name,
+				password: body.password,
+				photo: body.photo,
+				role: body.role,
+			}
+		});
 
-        return user;
-    }
+		return user;
+	}
 }
 
 export default new UserService();
