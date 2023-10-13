c4130090f3d040d2412c4b745ffe99149dce4b67
Maria Eduarda <dudah.s3@gmail.com>
2023-10-13 20:53:21 -0300

    feat: adiciona método delete de Artist

--- src/domains/Artist/service/ArtistService.ts
+++ src/domains/Artist/service/ArtistService.ts
@@ -5,9 +5,16 @@ import { Artist } from '@prisma/client'
 class ArtistService {
 	async readArtist(body: Artist) {
 		const artist = await prisma.artist.findMany({
-			where: { name: body.name}
+			where: { name: body.name }
 		});
-        return artist;
+		return artist;
+	}
+
+	async deleteArtist(body: Artist) {
+		const deletedArtist = await prisma.artist.delete({
+			where: { id: body.id }
+		});
+		return deletedArtist;
 	}
 
 }
