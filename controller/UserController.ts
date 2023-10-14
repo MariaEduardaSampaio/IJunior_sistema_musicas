4ba426ac2224a72e13eb692083f9aac897a5980e
victory <victoryujiy@hotmail.com>
2023-10-14 15:20:12 -0300

    Services

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -1,7 +1,6 @@
 import { User } from '@prisma/client';
 import UserService from '../src/domains/User/services/UserService';
 
-
 export async function createEntities(body: User) {
     try {
         await UserService.create(body)
