ed63e1fc66f952c6e1c62f671f9c87b176b62839
Maria Eduarda <dudah.s3@gmail.com>
2023-10-15 12:53:53 -0300

    feat: adiciona update no controller de Music

--- src/domains/Music/controller/index.ts
+++ src/domains/Music/controller/index.ts
@@ -1,13 +1,21 @@
 import { Music } from '@prisma/client';
 import MusicService from '../service/MusicService';
 
+export async function updateMusic(body: Music) {
+    try {
+        await MusicService.updateMusic(body)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
 export async function deleteMusic(body: Music) {
     try {
         await MusicService.deleteMusic(body.id)
-       } 
-       catch (error) 
-       {
+    }
+    catch (error) {
         console.log(error)
-       }
-           
+    }
+
 }
