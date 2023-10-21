fad4548bef1a8f0e3832784f7982485150894799
Maria Eduarda <dudah.s3@gmail.com>
2023-10-21 18:07:34 -0300

    fix: retira funçoes desnecessarias de user

--- src/domains/User/controller/index.ts
+++ src/domains/User/controller/index.ts
@@ -1,4 +1,4 @@
-import { User } from '@prisma/client';
+
 import UserService from '../services/UserService';
 
 import { Router, Request, Response, NextFunction } from 'express';
@@ -31,40 +31,5 @@ router.get('/email/:email', async (req: Request, res: Response, next: NextFuncti
 		next(error);
 	}
 });
-
-export async function readUserByID(id: number) {
-	try {
-		return await UserService.readUserByID(id);
-	}
-	catch (error) {
-		console.log(error);
-	}
-}
-
-export async function readUserByEmail(email: string) {
-	try {
-		return await UserService.readByEmail(email);
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-export async function updateUser(body: User) {
-	try {
-		return await UserService.updateUser(body);
-	}
-	catch (error) {
-		console.log(error);
-	}
-}
-
-export async function deleteUser(id: number) {
-	try {
-		return await UserService.deleteUser(id);
-	}
-	catch (error) {
-		console.log(error);
-	}
-}
 
 export default router;
