00c7218a46e4483c044911100351d9b8213a1f3d
Maria Eduarda <dudah.s3@gmail.com>
2023-10-21 16:55:21 -0300

    feat: adiciona rotas de get para user

--- src/domains/User/controller/index.ts
+++ src/domains/User/controller/index.ts
@@ -5,22 +5,27 @@ import { Router, Request, Response, NextFunction } from 'express';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
 	try {
-
-		res.send('Running');
-
-
-		// const users = await UserService.readByEmail('test@test.com');
-		// res.status(200).json(users);
+		const user = await UserService.create(req.body);
+		res.status(200).json(user);
 	} catch (error) {
 		next(error);
 	}
 });
 
-router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/id/:id', async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const user = await UserService.create(req.body);
+		const user = await UserService.readUserByID(Number(req.params.id));
+		res.status(200).json(user);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.get('/email/:email', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const user = await UserService.readByEmail(req.params.email);
 		res.status(200).json(user);
 	} catch (error) {
 		next(error);
