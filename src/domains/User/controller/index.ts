45760f31d00f4a196d273565abc0c6a54f57d939
Maria Eduarda <dudah.s3@gmail.com>
2023-10-22 12:39:02 -0300

    fix: resolve conflito e regras do eslint

--- src/domains/User/controller/index.ts
+++ src/domains/User/controller/index.ts
@@ -1,23 +1,13 @@
 import UserService from '../services/UserService';
 
 import { Router, Request, Response, NextFunction } from 'express';
-import { parse } from 'path';
 
 const router = Router();
 
 router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
 	try {
-<<<<<<< HEAD
-		const id = req.params.id;
-		// res.send('Running')
-		const users = await UserService.readUserByID(Number(id));
-		res.status(200).json(users);
-		// const users = await UserService.readByEmail('test@test.com');
-		// res.status(200).json(users);
-=======
 		const user = await UserService.create(req.body);
 		res.status(200).json(user);
->>>>>>> 204846b94fe425d9f680ab09912a1d33a7be798c
 	} catch (error) {
 		next(error);
 	}
@@ -32,38 +22,33 @@ router.get('/id/:id', async (req: Request, res: Response, next: NextFunction) =>
 	}
 });
 
-router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/email/:email', async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		await UserService.create(req.body);
-		res.json("Usuário criado com sucesso");
-
-	} catch (error){
+		const user = await UserService.readByEmail(req.params.email);
+		res.status(200).json(user);
+	} catch (error) {
 		next(error);
 	}
 });
 
-<<<<<<< HEAD
-router.put ('/', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const {id, name, email, photo, password, role }= req.body
-		if (req.body.id === undefined) {
-			res.status(404).json("Usuário não encontrado");
-		}
-		await UserService.updateUser({id: parseInt(id), name, email, photo, password, role});
-		res.status(200).json({id: parseInt(id), name, email, photo, password, role});
-	} catch (error){ 
-	  next(error);
-	}
-  });
-  
+		await UserService.create(req.body);
+		res.json('Usuário criado com sucesso');
 
-export async function createUser(body: User) {
-=======
-router.get('/email/:email', async (req: Request, res: Response, next: NextFunction) => {
->>>>>>> 204846b94fe425d9f680ab09912a1d33a7be798c
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.put('/', async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const user = await UserService.readByEmail(req.params.email);
-		res.status(200).json(user);
+		const { id, name, email, photo, password, role } = req.body;
+		if (req.body.id === undefined) {
+			res.status(404).json('Usuário não encontrado');
+		}
+		await UserService.updateUser({ id: parseInt(id), name, email, photo, password, role });
+		res.status(200).json({ id: parseInt(id), name, email, photo, password, role });
 	} catch (error) {
 		next(error);
 	}
