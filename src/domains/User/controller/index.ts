bc15bc99bd8cca41304cfc762f5aa00153f5cba9
Maria Eduarda <dudah.s3@gmail.com>
2023-10-22 12:43:18 -0300

    fix: resolve conflito e regras do eslint

--- src/domains/User/controller/index.ts
+++ src/domains/User/controller/index.ts
@@ -8,6 +8,7 @@ router.post('/create', async (req: Request, res: Response, next: NextFunction) =
 	try {
 		const user = await UserService.create(req.body);
 		res.status(200).json(user);
+		res.json('Usuário criado com sucesso');
 	} catch (error) {
 		next(error);
 	}
@@ -26,16 +27,6 @@ router.get('/email/:email', async (req: Request, res: Response, next: NextFuncti
 	try {
 		const user = await UserService.readByEmail(req.params.email);
 		res.status(200).json(user);
-	} catch (error) {
-		next(error);
-	}
-});
-
-router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await UserService.create(req.body);
-		res.json('Usuário criado com sucesso');
-
 	} catch (error) {
 		next(error);
 	}
