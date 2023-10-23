93877432e438a5eda9ecef9fb5ccaf1573b79364
victory <victoryujiy@hotmail.com>
2023-10-22 21:49:39 -0300

    Artist controller

--- src/domains/Artist/controller/index.ts
+++ src/domains/Artist/controller/index.ts
@@ -4,6 +4,18 @@ import { Router, Request, Response, NextFunction } from 'express';
 
 const router = Router();
 
+
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const artist = await ArtistService.createArtist(req.body);
+		res.status(201).json(artist);
+	}
+	catch (error) {
+		next(error);
+	}
+});
+
+
 router.put('/', async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const { streams, id, ...rest } = req.body;
@@ -16,6 +28,27 @@ router.put('/', async (req: Request, res: Response, next: NextFunction) => {
 	}
 });
 
+router.get('/id/:id', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const artists = await ArtistService.readArtistByID(Number(req.params.id));
+		res.status(200).json(artists);
+	}
+	catch (error) {
+		next(error);
+	}
+});
+
+router.get('/name/:name', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const artists = await ArtistService.readArtistByName(String(req.params.name));
+		res.status(200).json(artists);
+	}
+	catch (error) {
+		next(error);
+	}
+});
+
+
 router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const artist = await ArtistService.deleteArtist(parseInt(req.params.id));
@@ -24,5 +57,7 @@ router.delete('/:id', async (req: Request, res: Response, next: NextFunction) =>
 		next(error);
 	}
 });
+
+
 
 export default router;
