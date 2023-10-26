a0d8a0836c8f3bb334f277b6661b958048beb7f6
Maria Eduarda <dudah.s3@gmail.com>
2023-10-26 15:12:06 -0300

    feat: adiciona middlewares de autenticação de usuario

---
+++ src/middlewares/auth-middlewares.ts
@@ -0,0 +1,73 @@
+import { Request, Response, NextFunction } from 'express';
+import { statusCodes } from '../../utils/constants/statusCodes';
+import { PermissionError } from '../../errors/PermissionError';
+import { User } from '@prisma/client';
+import UserService from '../domains/User/services/UserService';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+function generateJWT(user: User, res: Response) {
+	const body = {
+		id: user.id,
+		name: user.name,
+		email: user.email,
+		role: user.role,
+	};
+
+	const token = jwt.sign({ user: body }, process.env.SECRET_KEY as string,
+		{ expiresIn: process.env.JWT_EXPIRATION });
+
+	res.cookie('jwt', token, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV !== 'development',
+	});
+}
+
+function cookieExtractor(req: Request) {
+	let token = null;
+	if (req && req.cookies) {
+		token = req.cookies['jwt'];
+	}
+	return token;
+}
+
+async function loginMiddleware(req: Request, res: Response, next: NextFunction) {
+	try {
+		const user = await UserService.readByEmail(req.body.email);
+		if (!user) {
+			throw new PermissionError('Email e/ou senha incorretos.');
+		} else {
+			const matchingPassword = await bcrypt.compare(req.body.password, user.password);
+			if (!matchingPassword) {
+				throw new PermissionError('Email e/ou senha incorretos.');
+			}
+		}
+
+		generateJWT(user, res);
+
+		res.status(statusCodes.NO_CONTENT).end();
+	} catch (error) {
+		next(error);
+	}
+}
+
+// async function notLoggedIn(req: Request, res: Response, next: NextFunction){}
+
+function verifyJWT(req: Request, res: Response, next: NextFunction) {
+	try {
+		const token = cookieExtractor(req);
+		if (token) {
+			const decoded = jwt.verify(token, process.env.SECRET_KEY as string);
+			req.body.user = decoded;
+		}
+		if (!req.body.user) {
+			throw new PermissionError('Você precisa estar logado para realizar esta ação.');
+		}
+		next();
+	} catch (error) {
+		next(error);
+	}
+}
+
+
+export default { loginMiddleware, verifyJWT }; // adicionar notLoggedIn
