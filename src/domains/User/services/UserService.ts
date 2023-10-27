d902f06396114b1af62b20c8796d05845c9c79d0
Marcos Daniel <marcosnumber11@gmail.com>
2023-10-27 07:52:30 -0300

    feat:Encrypting the password

--- src/domains/User/services/UserService.ts
+++ src/domains/User/services/UserService.ts
@@ -1,9 +1,18 @@
 import prisma from '../../../../config/client';
 import { User } from '@prisma/client';
-
+import bcrypt from 'bcrypt';
 
 class UserService {
+	async encryptPassword(password: string) {
+		const salt = await bcrypt.genSalt(10);
+		const encryptedPassword = await bcrypt.hash(password, salt);
+		return encryptedPassword;
+	}
+
 	async create(body: User) {
+
+		body.password = await this.encryptPassword(body.password);
+
 		const user = await prisma.user.create({
 			data: {
 				email: body.email,
@@ -45,6 +54,9 @@ class UserService {
 	}
 
 	async updateUser(body: User) {
+
+		if(body.password) body.password = await this.encryptPassword(body.password);
+
 		const user = await prisma.user.update({
 			data: {
 				name: body.name,
