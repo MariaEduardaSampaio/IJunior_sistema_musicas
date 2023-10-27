eec425ff1c2595e41ab0877bb98b915dfb750053
Marcos Daniel <marcosnumber11@gmail.com>
2023-10-27 14:56:07 -0300

    feat: basic check roles middleware

--- src/middlewares/checkRole.ts
+++ src/middlewares/checkRole.ts
@@ -1,2 +1,18 @@
 // Analisar req.role e req.user.role
 // Chamar next() ou retornar erro de permissao
+import { NextFunction, Request, Response } from 'express';
+import statusCodes from '../../utils/constants/statusCodes';
+import UserRoles from '../../utils/constants/userRoles';
+
+function checkRoles(req: Request, res: Response, next: NextFunction) {
+	const role = req.body.role;
+
+	if (role === UserRoles.ADMIN) {
+		next(); // OK - pode continuar
+	} else {
+		res.status(statusCodes.FORBIDDEN).json('Você não tem permissão para acessar essa rota.');
+	}
+}
+
+
+export default checkRoles;
