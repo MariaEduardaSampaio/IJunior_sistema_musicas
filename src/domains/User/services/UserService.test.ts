6af0227920f8d748c5ea75c2e21c857b8ce07a8a
Maria Eduarda <dudah.s3@gmail.com>
2023-11-12 15:57:08 -0300

    testes: add testes de user

--- src/domains/User/services/UserService.test.ts
+++ src/domains/User/services/UserService.test.ts
@@ -13,7 +13,7 @@ describe('create', () => {
 
 	test('os dados do usuário são passados como entrada ==> cria esse usuario no banco', async () => {
 		// ARRANGE
-		
+
 		const mockEncryptedPassword = 'encryptedPassword';
 		const mockPassword = 'password123';
 
@@ -29,7 +29,7 @@ describe('create', () => {
 		// A função encryptPassword vai retornar sempre 'encryptedPassword'
 		const encryptedPasswordSpy = jest.spyOn(UserService, 'encryptPassword')
 			.mockResolvedValue(mockEncryptedPassword);
-			
+
 		// Não temos esse usuario cadastrado no banco
 		const findUniqueSpy = jest.spyOn(prisma.user, 'findUnique')
 			.mockResolvedValue(null);
@@ -56,13 +56,13 @@ describe('create', () => {
 
 		expect(createSpy).toHaveBeenCalledWith({
 			data: {
-				... mockUser
+				...mockUser
 			},
 		});
 		expect(createSpy).toHaveBeenCalledTimes(1);
 
 		expect(createdUser).toEqual({
-			... mockUser,
+			...mockUser,
 			password: mockEncryptedPassword,
 		});
 	});
@@ -82,7 +82,7 @@ describe('create', () => {
 		// A função encryptPassword vai retornar sempre 'encryptedPassword'
 		jest.spyOn(UserService, 'encryptPassword')
 			.mockResolvedValue(mockEncryptedPassword);
-			
+
 		// Temos esse usuario cadastrado no banco
 		jest.spyOn(prisma.user, 'findUnique')
 			.mockResolvedValue({
@@ -93,14 +93,14 @@ describe('create', () => {
 		// Não deve ser chamada
 		jest.spyOn(prisma.user, 'create')
 			.mockResolvedValue({
-				... mockUser, 
+				...mockUser,
 				password: mockEncryptedPassword
 			});
 
 		// ACT & ASSERT
 
 		// Esperamos que UserService.create lance uma exceção de QueryError
-		return expect(async ()=>{
+		return expect(async () => {
 			await UserService.create(mockUser);
 		}).rejects.toThrow(new QueryError('Email já cadastrado!'));
 	});
@@ -110,25 +110,25 @@ describe('create', () => {
 		const mockUser = {} as User;
 
 		// ACT & ASSERT
-		expect(async ()=> { await UserService.create(mockUser); })
+		expect(async () => { await UserService.create(mockUser); })
 			.rejects.toThrow(new InvalidParamError('Campos obrigatórios não preenchidos.'));
 
 		mockUser.id = 1;
-		expect(async ()=> { await UserService.create(mockUser); })
+		expect(async () => { await UserService.create(mockUser); })
 			.rejects.toThrow(new InvalidParamError('Campos obrigatórios não preenchidos.'));
 
 		mockUser.email = 'lucas-example@gmail.com';
-		expect(async ()=> { await UserService.create(mockUser); })
+		expect(async () => { await UserService.create(mockUser); })
 			.rejects.toThrow(new InvalidParamError('Campos obrigatórios não preenchidos.'));
 
 		mockUser.password = 'password123';
-		expect(async ()=> { await UserService.create(mockUser); })
+		expect(async () => { await UserService.create(mockUser); })
 			.rejects.toThrow(new InvalidParamError('Campos obrigatórios não preenchidos.'));
 
 		mockUser.name = 'Lucas Paulo';
-		expect(async ()=> { await UserService.create(mockUser); })
+		expect(async () => { await UserService.create(mockUser); })
 			.rejects.toThrow(new InvalidParamError('Campos obrigatórios não preenchidos.'));
-		
+
 	});
 
 	test('usuário com senha vazia ==> lança uma exceção de InvalidParam', async () => {
@@ -142,14 +142,191 @@ describe('create', () => {
 		} as User;
 
 		// ACT & ASSERT
-		expect(async ()=> { await UserService.create(mockUser); })
+		expect(async () => { await UserService.create(mockUser); })
 			.rejects.toThrow(new InvalidParamError('Campos obrigatórios não preenchidos.'));
 
 	});
 });
 
 describe('deleteUser', () => {
+	beforeEach(() => {
+		jest.restoreAllMocks();
+		jest.clearAllMocks();
+	});
 
+	test('Não existe usuário com o ID passado => lança exceção',
+		async () => {
+			const invalidID = 2;
+			const invalidUser = {
+				id: 1,
+				email: 'tobias@hotmail.com',
+				name: 'tobias',
+				password: '#tobias123#',
+				photo: 'tobias.jpg',
+				role: 'admin',
+			} as User;
+
+			const findUniqueSpy = jest.spyOn(prisma.user, 'findUnique').mockRejectedValue(null);
+
+			const deletedUser = await UserService.deleteUser(invalidID);
+
+			expect(findUniqueSpy).toHaveBeenCalledWith(invalidUser.id);
+			expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+			expect(deletedUser).rejects.toThrow('Usuário com este ID não foi encontrado.');
+		});
+
+	test('ID não é um número inteiro => lança exceção',
+		async () => {
+			const invalidUser = {
+				id: 4.2,
+				email: 'junior@hotmail.com',
+				name: 'Junior',
+				password: '#junior123#',
+				photo: 'junior.jpg',
+				role: 'user',
+			} as User;
+
+			const deletedUser = await UserService.deleteUser(invalidUser.id);
+
+			expect(deletedUser).rejects.toThrow('ID deve ser um número válido.');
+		});
+
+	test('ID é um número válido que existe no banco => deleta usuário',
+		async () => {
+			const validUser = {
+				id: 1,
+				email: 'gustavo@hotmail.com',
+				name: 'gustavo',
+				password: '#gustavo123#',
+				photo: 'gustavo.jpg',
+				role: 'admin',
+			} as User;
+
+			const findUniqueSpy = jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(validUser);
+			const deleteSpy = jest.spyOn(prisma.user, 'delete').mockResolvedValue(validUser);
+
+			const deletedUser = await UserService.deleteUser(validUser.id);
+
+			expect(findUniqueSpy).toHaveBeenCalledWith(validUser.id);
+			expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+			expect(deleteSpy).toHaveBeenCalledWith(validUser.id);
+			expect(deleteSpy).toHaveBeenCalledTimes(1);
+			expect(deletedUser).toBe(validUser);
+		});
+});
+
+describe('updateUser', () => {
+	beforeEach(() => {
+		jest.restoreAllMocks();
+		jest.clearAllMocks();
+	});
+
+	test('Tenta atualizar o email => lança exceção',
+		async () => {
+			const mockEncryptedPassword = 'encryptedPassword';
+			const mockUser = {
+				id: 1,
+				email: 'joao@dominio.com',
+				name: 'joao',
+				password: mockEncryptedPassword,
+				role: 'admin',
+			} as User;
+
+			const findUniqueSpy = jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+
+			const updatedUser = await UserService.updateUser(mockUser);
+
+			expect(findUniqueSpy).toHaveBeenCalledWith(mockUser.id);
+			expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+			expect(updatedUser).rejects.toThrow('Não é possível atualizar o email.');
+		});
+
+	describe('Não preenche parâmetros obrigatórios => lança exceção',
+		async () => {
+			const invalidUser = {
+				id: 1,
+				email: 'email@teste.br',
+				name: 'nome do usuário',
+				password: 'senha do usuário',
+				role: 'user',
+			} as User;
+
+			const findUniqueSpy = jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(invalidUser);
+
+			test('Não preenche nome => lança exceção',
+				async () => {
+					invalidUser.name = '';
+
+					const updatedUser = await UserService.updateUser(invalidUser);
+
+					expect(findUniqueSpy).toHaveBeenCalledWith(invalidUser.id);
+					expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+					expect(updatedUser).rejects.toThrow('Campos obrigatórios não preenchidos.');
+
+				});
+
+			test('Não preenche email => lança exceção',
+				async () => {
+					invalidUser.email = '';
+
+					const updatedUser = await UserService.updateUser(invalidUser);
+
+					expect(findUniqueSpy).toHaveBeenCalledWith(invalidUser.id);
+					expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+					expect(updatedUser).rejects.toThrow('Campos obrigatórios não preenchidos.');
+
+				});
+
+			test('Não preenche senha => lança exceção',
+				async () => {
+					invalidUser.password = '';
+
+					const updatedUser = await UserService.updateUser(invalidUser);
+
+					expect(findUniqueSpy).toHaveBeenCalledWith(invalidUser.id);
+					expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+					expect(updatedUser).rejects.toThrow('Campos obrigatórios não preenchidos.');
+
+				});
+
+			test('Não preenche cargo => lança exceção',
+				async () => {
+					invalidUser.role = '';
+
+					const updatedUser = await UserService.updateUser(invalidUser);
+
+					expect(findUniqueSpy).toHaveBeenCalledWith(invalidUser.id);
+					expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+					expect(updatedUser).rejects.toThrow('Campos obrigatórios não preenchidos.');
+
+				});
+		});
+
+	test('Passa os parâmetros corretamente => atualiza usuário',
+		async () => {
+			const mockEncryptedPassword = 'encryptedPassword';
+			const validUser = {
+				id: 1,
+				email: 'lucas@dominio.com',
+				name: 'lucas',
+				password: mockEncryptedPassword,
+				role: 'admin',
+			} as User;
+
+			const findUniqueSpy = jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(validUser);
+			const encryptedPasswordSpy = jest.spyOn(UserService, 'encryptPassword').mockResolvedValue(mockEncryptedPassword);
+			const updateSpy = jest.spyOn(prisma.user, 'update').mockResolvedValue(validUser);
+
+			const updatedUser = await UserService.updateUser(validUser);
+
+			expect(findUniqueSpy).toHaveBeenCalledWith(validUser.email);
+			expect(findUniqueSpy).toHaveBeenCalledTimes(1);
+			expect(encryptedPasswordSpy).toHaveBeenCalledWith(validUser.password);
+			expect(encryptedPasswordSpy).toHaveBeenCalledTimes(1);
+			expect(updateSpy).toHaveBeenCalledWith({ where: { id: validUser.id }, data: validUser });
+			expect(updateSpy).toHaveBeenCalledTimes(1);
+			expect(updatedUser).toBe(validUser);
+		});
 });
 
 describe('readUserByID', () => {
@@ -167,14 +344,14 @@ describe('encryptPassword', () => {
 		const mockPassword = 'password123';
 		const mockEncryptedPassword = 'encryptedPassword';
 		const mockSalt = 10;
-		const mockGenSalt = 'mockGenSalt'; 
-		
+		const mockGenSalt = 'mockGenSalt';
+
 		const saltSpy = jest.spyOn(bcrypt, 'genSalt')
 			.mockImplementation(() => Promise.resolve(mockGenSalt));
 
-		const bcryptSpy = jest.spyOn(bcrypt , 'hash')
-			.mockImplementation(()=> Promise.resolve(mockEncryptedPassword));
-		
+		const bcryptSpy = jest.spyOn(bcrypt, 'hash')
+			.mockImplementation(() => Promise.resolve(mockEncryptedPassword));
+
 		// ACT
 		const encryptedPassword = await UserService.encryptPassword(mockPassword);
 
@@ -194,9 +371,5 @@ describe('encryptPassword', () => {
 });
 
 describe('read', () => {
-
-});
-
-describe('updateUser', () => {
 
 });
